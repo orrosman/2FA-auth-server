28fa5d94e4982dcd20a527237c8b8dbe2e5e0671
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-12-12 23:45:01 +0200

    added logo and fixed header text

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import exampleLogo from '../assets/logo.svg';
+import exampleLogo from '../assets/2fa-logo.svg';
 
 function Header() {
 	return (
 		<nav className="navbar navbar-dark bg-dark">
 			<i className="navbar-brand mx-auto text-center">
-				<img
-					src={exampleLogo}
-					width="100"
-					height="100"
-					alt="example logo"
-				></img>
-				<div>header text here</div>
+				<img src={exampleLogo} width="100" height="100" alt="2FA Project"></img>
+				<div>2FA Project</div>
 			</i>
 		</nav>
 	);
