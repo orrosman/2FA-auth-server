ff2a6be9e9eb7e0230747c02487676a66a9b1cce
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-12-13 00:08:49 +0200

    added endpoint to change 2FA to on/off

--- client/src/server-requests.ts
+++ client/src/server-requests.ts
@@ -19,3 +19,10 @@ export const login = async (credentials: UserCredentials) => {
 	const response = await axios.post(`${BASE_API_URL}/user/login`, credentials);
 	return response;
 };
+
+export const twoFactorAuth = async (email: string) => {
+	const response = await axios.post(`${BASE_API_URL}/user/2FA`, {
+		email: email,
+	});
+	return response;
+};
