383cac93303154dd118b5118a048765dee768528
Or Rosman <42673555+orrosman@users.noreply.github.com>
2021-12-12 18:50:32 +0200

    Basic page with header and footer

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 ReactDOM.render(
 	<React.StrictMode>
+		<Header />
 		<App />
+		<Footer />
 	</React.StrictMode>,
 	document.getElementById('root')
 );
